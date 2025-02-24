9addc0f6470592c8dc37bb0058354968960c772d
User-207 <Luque3435@outlook.com>
2025-02-23 21:48:04 -0300

    Add files via upload

---
+++ Aula 17022025/ProgramEX2.1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    public class Caneta
+    {
+        private string cor;
+        private bool tampada;
+
+        public Caneta(string cor)
+        {
+            this.cor = cor;
+            this.tampada = true;
+        }
+
+        public void Destampar()
+        {
+            if (tampada)
+            {
+                tampada = false;
+                Console.WriteLine("A caneta foi destampada");
+            }
+            else
+            {
+                Console.WriteLine("A caneta já está destampada");
+            }
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caneta C1 = new Caneta("vermelha");
+            C1.Destampar();
+            Caneta C2 = new Caneta("azul");
+            C2.Destampar();
+
+    }
+}
+}
