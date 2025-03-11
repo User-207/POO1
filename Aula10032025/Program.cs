7496c430f47cca72407d4ba64be1e1d4397bb2c3
User-207 <Luque3435@outlook.com>
2025-03-10 21:46:24 -0300

    Add files via upload

---
+++ Aula10032025/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2___10_03_2025
+{
+    public class Carro
+    { 
+        public string marca;
+        public string nome;
+        public double velocidadefinal;
+        public double velocidadeinicial;
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
